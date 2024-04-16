d9e79d0fbeef9a71de2f7e9e2410f35b6816486f
wildFloricica <wildFloricica@beautifull.garden>
2024-04-16 16:19:28 +0300

    added multiple animation for logo + change on mouseenter

--- pages/close_btn/main.js
+++ pages/close_btn/main.js
@@ -1,2 +1,18 @@
-document.querySelector("#logo-floricica").onclick = () =>
-  window.FloricicaAPI.closeApp();
+const logo = document.querySelector("#logo-floricica");
+logo.onclick = () => window.FloricicaAPI.closeApp();
+
+const animations = ["dissapear", "heartbeat", "patrol"];
+const def_classes = "prevent-select";
+
+var counter = 0;
+
+logo.onmouseenter = () => {
+  if (counter++ < 4) return;
+  logo.className = def_classes;
+  setTimeout(() => {
+    logo.className =
+      def_classes +
+      " " +
+      animations[Math.floor(Math.random() * animations.length)];
+  }, 1000);
+};
